3f9e2535610042a4a02c2ad067b4a41b17d5a541
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-05-06 13:36:35 +0400

    Routing 1

---
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public CompaniesController(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //// If you want to route specific index
+        //// But you have to comment [Route("api/companies")] 
+        //[Route("api/companies/getcompanies")]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action { ex }");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Entities.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Contracts
 {
     public interface ICompanyRepository
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
