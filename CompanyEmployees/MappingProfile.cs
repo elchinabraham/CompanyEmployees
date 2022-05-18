18bde459394bdadd8f75ee2e4c12daa689a77253
Elchin Abraham <elchinibrahimli8@gmail.com>
2022-05-18 13:42:11 +0400

    PUT request 2022.05.18 13:42

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -12,6 +12,9 @@ namespace CompanyEmployees
             CreateMap<Employee, EmployeeDto>();
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+            CreateMap<EmployeeForUpdateDto, Employee>();
+            CreateMap<CompanyForUpdateDto, Company>();
+
         }
     }
 }
